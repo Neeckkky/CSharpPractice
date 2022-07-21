a430502bbdbb5ac44e0cba22c7aa7b929c774adf
Nickkky <workingnick25@yandex.ru>
2022-07-21 18:31:23 +0300

    Сделал первое задание на вычисление квадратного корня

---
+++ SquareRootOfNumber/SquareRootOfNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число для вычисления квадратного корня: ");
+string changingNumber = Console.ReadLine();
+double num;
+
+bool correctValue = double.TryParse(changingNumber, out num);
+
+if (correctValue)
+{
+    double rootResult = Math.Sqrt(num);
+    if (num < 0)
+    {
+        Console.WriteLine("Ошибка: корень отрицального числа вычислить невозможно.");
+    }
+  else
+    {
+        Console.WriteLine("Квадратный корень числа " + num + " равен " + rootResult);
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка: " + changingNumber + " не является числом.");
+}
+
+
