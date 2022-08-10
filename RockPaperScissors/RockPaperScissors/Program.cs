ababed783448f252d9b802c2d146a3a270a85fdc
Nickkky <workingnick25@yandex.ru>
2022-08-10 15:35:05 +0300

    Задание "Камень Ножницы Бумага"

---
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information 
+while (true)
+{
+    Random rnd = new Random();
+    int RndRPS = rnd.Next(0, 3);
+    RPS ValueRPS = (RPS)(int)Enum.GetValues(typeof(RPS)).GetValue(RndRPS);
+
+    Console.Write("Rock paper or scissorss?: ");
+    int intRPS = int.Parse(Console.ReadLine());
+
+
+
+    if (intRPS == 0 && (int)ValueRPS == 1)
+    {
+        Console.WriteLine("Победил игрок!");
+    }
+
+    else if (intRPS == 1 && (int)ValueRPS == 0)
+    {
+        Console.WriteLine("Победил игрок!");
+    }
+
+    else if (intRPS == 2 && (int)ValueRPS == 1)
+    {
+        Console.WriteLine("Победил игрок!");
+    }
+
+
+    else if (intRPS == 2 && ValueRPS == 0)
+    {
+        Console.WriteLine("Победил компьютер!");
+    }
+
+
+    else if (intRPS == 0 && (int)ValueRPS == 2)
+    {
+        Console.WriteLine("Победил компьютер!");
+    }
+
+    else if (intRPS == 1 && (int)ValueRPS == 2)
+    {
+        Console.WriteLine("Победил компьютер!");
+    }
+
+
+
+    else if (intRPS == (int)ValueRPS)
+    {
+        Console.WriteLine("Ничья");
+    }
+}
+
+
+
+enum RPS : int
+{ ROCK, PAPER, SCISSORS }
+
+
+
+
+
+
---
+++ RockPaperScissors/dfd/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Numbers one = (Numbers)Enum.Parse(typeof(Numbers), "one");
+Numbers two = (Numbers)Enum.Parse(typeof(Numbers), "two");
+Numbers three = (Numbers)Enum.Parse(typeof(Numbers), "three");
+Numbers four = (Numbers)Enum.Parse(typeof(Numbers), "four");
+
+
+enum Numbers
+{ one = 20, two = 10, three = 30, four = 5 }
+Console.Write("{0}\n{1}\n{2}\n{3}\n", one, two, three, four);
+Console.WriteLine("Выберите число:");
+int mb = Convert.ToInt32(Console.Readline());
