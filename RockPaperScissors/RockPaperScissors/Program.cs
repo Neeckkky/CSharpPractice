46f8ec63b72c78915eeb54b3ce27c80b59bf5e3d
Nickkky <workingnick25@yandex.ru>
2022-08-26 18:52:25 +0300

    Задание xml про сотрудников и немного отредактированное задание про игру

--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -1,58 +1,70 @@
 ﻿// See https://aka.ms/new-console-template for more information 
+Random rnd = new Random();
+int RndRPS = rnd.Next(3);
+Console.WriteLine("Добро пожаловать в игру 'Rock Paper Scissors'! Вам следует выбрать один из 3 вариантов, где 0 - ROCK, 1 - PAPER, 2 - SCISSORS. Удачи!");
+
 while (true)
 {
-    Random rnd = new Random();
-    int RndRPS = rnd.Next(0, 3);
-    RPS ValueRPS = (RPS)(int)Enum.GetValues(typeof(RPS)).GetValue(RndRPS);
+    PlayerChoice ComputerChoice = (PlayerChoice)Enum.GetValues(typeof(PlayerChoice)).GetValue(RndRPS);
 
-    Console.Write("Rock paper or scissorss?: ");
-    int intRPS = int.Parse(Console.ReadLine());
+   
 
 
-
-    if (intRPS == 0 && (int)ValueRPS == 1)
-    {
-        Console.WriteLine("Победил игрок!");
-    }
-
-    else if (intRPS == 1 && (int)ValueRPS == 0)
-    {
-        Console.WriteLine("Победил игрок!");
-    }
-
-    else if (intRPS == 2 && (int)ValueRPS == 1)
-    {
-        Console.WriteLine("Победил игрок!");
-    }
+    Console.Write("Введите число: ");
+    int userChoice = (int.Parse(Console.ReadLine()));
+    Console.WriteLine("Пользователь ввел: " + userChoice);
+    Console.WriteLine("Компьютер ввел: " + (int)ComputerChoice);
 
 
-    else if (intRPS == 2 && ValueRPS == 0)
+    if ((int)userChoice == (int)PlayerChoice.ROCK && (int)ComputerChoice == (int)PlayerChoice.PAPER)
     {
         Console.WriteLine("Победил компьютер!");
     }
 
+    else if (userChoice == (int)PlayerChoice.ROCK && (int)ComputerChoice == (int)PlayerChoice.SCISSORS)
+    {
+        Console.WriteLine("Победил игрок!");
+    }
 
-    else if (intRPS == 0 && (int)ValueRPS == 2)
+
+    else if (userChoice == (int)PlayerChoice.PAPER && (int)ComputerChoice == (int)PlayerChoice.ROCK)
+    {
+        Console.WriteLine("Победил игрок!");
+    }
+
+    else if (userChoice == (int)PlayerChoice.PAPER && (int)ComputerChoice == (int)PlayerChoice.SCISSORS)
     {
         Console.WriteLine("Победил компьютер!");
     }
 
-    else if (intRPS == 1 && (int)ValueRPS == 2)
+    else if (userChoice == (int)PlayerChoice.SCISSORS && (int)ComputerChoice == (int)PlayerChoice.ROCK)
     {
         Console.WriteLine("Победил компьютер!");
     }
 
-
-
-    else if (intRPS == (int)ValueRPS)
+    else if (userChoice == (int)PlayerChoice.SCISSORS && (int)ComputerChoice == (int)PlayerChoice.PAPER)
     {
-        Console.WriteLine("Ничья");
+        Console.WriteLine("Победил игрок!");
+    }
+
+    else if (userChoice == (int)PlayerChoice.ROCK && (int)ComputerChoice == (int)PlayerChoice.ROCK)
+    {
+        Console.WriteLine("Ничья!");
+    }
+
+    else if (userChoice == (int)PlayerChoice.PAPER && (int)ComputerChoice == (int)PlayerChoice.PAPER)
+    {
+        Console.WriteLine("Ничья!");
+    }
+
+    else if (userChoice == (int)PlayerChoice.SCISSORS && (int)ComputerChoice == (int)PlayerChoice.SCISSORS)
+    {
+        Console.WriteLine("Ничья!");
     }
 }
 
 
-
-enum RPS : int
+enum PlayerChoice
 { ROCK, PAPER, SCISSORS }
 
 
---
+++ XMLProject/XMLProject/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+using System.Xml;
+while (true)
+{
+    List<User> users = new List<User>();
+    Console.WriteLine("Введите имя пол и год рождения сотрудника:");
+    XmlDocument xmlDocument = new XmlDocument();
+    xmlDocument.Load(@"C:\Users\User1\source\repos\XMLProject\XMLProject\bin\Debug\net6.0\people.xml");
+    XmlElement newemployee = xmlDocument.DocumentElement;
+
+
+    XmlElement userElem = xmlDocument.CreateElement("user");
+    XmlAttribute nameAttr = xmlDocument.CreateAttribute("name");
+    XmlElement sexElem = xmlDocument.CreateElement("sex");
+    XmlElement birthyearElem = xmlDocument.CreateElement("birthyear");
+
+
+    string name = Convert.ToString(Console.ReadLine());
+    string sex = Convert.ToString(Console.ReadLine());
+    string birthyear = Convert.ToString(Console.ReadLine());
+
+    XmlText nameText = xmlDocument.CreateTextNode(name);
+    XmlText sexText = xmlDocument.CreateTextNode(sex);
+    XmlText birthyearText = xmlDocument.CreateTextNode(birthyear);
+
+    nameAttr.AppendChild(nameText);
+    sexElem.AppendChild(sexText);
+    birthyearElem.AppendChild(birthyearText);
+
+    userElem.Attributes.Append(nameAttr);
+    userElem.AppendChild(sexElem);
+    userElem.AppendChild(birthyearElem);
+    newemployee.AppendChild(userElem);
+    xmlDocument.Save("people.xml");
+
+    XmlDocument xml = new XmlDocument();
+    xml.Load(@"C:\Users\User1\source\repos\XMLProject\XMLProject\bin\Debug\net6.0\people.xml");
+    XmlElement element = xml.DocumentElement;
+    foreach (XmlElement xnode in element)
+    {
+        User user = new User();
+        XmlNode attr = xnode.Attributes.GetNamedItem("name");
+        if (attr != null)
+            user.Name = attr.Value;
+
+        foreach (XmlNode childnode in xnode.ChildNodes)
+        {
+            if (childnode.Name == "sex")
+                user.Sex = childnode.InnerText;
+            if (childnode.Name == "birthyear")
+                user.Birthyear = Int32.Parse(childnode.InnerText);
+        }
+        users.Add(user);
+    }
+    foreach (User emp in users)
+        Console.WriteLine($"{emp.Name}; {emp.Sex} пол - {emp.Birthyear} года рождения");
+    Console.ReadKey();
+
+}
+
+
+
+public class User
+{
+    public string Name;
+    public string Sex;
+    public int Birthyear;
+}
+
+
+
+
+
+
+
+
