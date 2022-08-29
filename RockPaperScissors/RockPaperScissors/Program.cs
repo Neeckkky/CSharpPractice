c4c1ae58c68c23fc8e506a63db9ce83d76a82ac0
Nickkky <workingnick25@yandex.ru>
2022-08-29 11:18:14 +0300

    no message

--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -1,63 +1,66 @@
 ﻿// See https://aka.ms/new-console-template for more information 
-Random rnd = new Random();
-int RndRPS = rnd.Next(3);
+
 Console.WriteLine("Добро пожаловать в игру 'Rock Paper Scissors'! Вам следует выбрать один из 3 вариантов, где 0 - ROCK, 1 - PAPER, 2 - SCISSORS. Удачи!");
 
 while (true)
 {
+    Random rnd = new Random();
+    int RndRPS = rnd.Next(3);
     PlayerChoice ComputerChoice = (PlayerChoice)Enum.GetValues(typeof(PlayerChoice)).GetValue(RndRPS);
 
-   
+    
+
+
 
 
     Console.Write("Введите число: ");
-    int userChoice = (int.Parse(Console.ReadLine()));
+    PlayerChoice userChoice = (PlayerChoice)int.Parse(Console.ReadLine());
     Console.WriteLine("Пользователь ввел: " + userChoice);
-    Console.WriteLine("Компьютер ввел: " + (int)ComputerChoice);
+    Console.WriteLine("Компьютер ввел: " + ComputerChoice);
 
 
-    if ((int)userChoice == (int)PlayerChoice.ROCK && (int)ComputerChoice == (int)PlayerChoice.PAPER)
+    if (userChoice == PlayerChoice.ROCK && ComputerChoice == PlayerChoice.PAPER)
     {
         Console.WriteLine("Победил компьютер!");
     }
 
-    else if (userChoice == (int)PlayerChoice.ROCK && (int)ComputerChoice == (int)PlayerChoice.SCISSORS)
+    else if (userChoice == PlayerChoice.ROCK && ComputerChoice == PlayerChoice.SCISSORS)
     {
         Console.WriteLine("Победил игрок!");
     }
 
 
-    else if (userChoice == (int)PlayerChoice.PAPER && (int)ComputerChoice == (int)PlayerChoice.ROCK)
+    else if (userChoice == PlayerChoice.PAPER && ComputerChoice == PlayerChoice.ROCK)
     {
         Console.WriteLine("Победил игрок!");
     }
 
-    else if (userChoice == (int)PlayerChoice.PAPER && (int)ComputerChoice == (int)PlayerChoice.SCISSORS)
+    else if (userChoice == PlayerChoice.PAPER && ComputerChoice == PlayerChoice.SCISSORS)
     {
         Console.WriteLine("Победил компьютер!");
     }
 
-    else if (userChoice == (int)PlayerChoice.SCISSORS && (int)ComputerChoice == (int)PlayerChoice.ROCK)
+    else if (userChoice == PlayerChoice.SCISSORS && ComputerChoice == PlayerChoice.ROCK)
     {
         Console.WriteLine("Победил компьютер!");
     }
 
-    else if (userChoice == (int)PlayerChoice.SCISSORS && (int)ComputerChoice == (int)PlayerChoice.PAPER)
+    else if (userChoice == PlayerChoice.SCISSORS && ComputerChoice == PlayerChoice.PAPER)
     {
         Console.WriteLine("Победил игрок!");
     }
 
-    else if (userChoice == (int)PlayerChoice.ROCK && (int)ComputerChoice == (int)PlayerChoice.ROCK)
+    else if (userChoice == PlayerChoice.ROCK && ComputerChoice == PlayerChoice.ROCK)
     {
         Console.WriteLine("Ничья!");
     }
 
-    else if (userChoice == (int)PlayerChoice.PAPER && (int)ComputerChoice == (int)PlayerChoice.PAPER)
+    else if (userChoice == PlayerChoice.PAPER && ComputerChoice == PlayerChoice.PAPER)
     {
         Console.WriteLine("Ничья!");
     }
 
-    else if (userChoice == (int)PlayerChoice.SCISSORS && (int)ComputerChoice == (int)PlayerChoice.SCISSORS)
+    else if (userChoice == PlayerChoice.SCISSORS && ComputerChoice == PlayerChoice.SCISSORS)
     {
         Console.WriteLine("Ничья!");
     }
